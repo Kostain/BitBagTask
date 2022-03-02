47a73c7f2eef612e45ec72ca9eba347b7e78cec8
Adam Zdanowski <adamzdan97@gmail.com>
2022-03-02 11:43:15 +0100

    Add unit test for product entity

---
+++ tests/Unit/Entity/Product/ProductTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Entity\Product;
+
+use App\Entity\Product\Product;
+use PHPUnit\Framework\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function testProductWithoutColorAttribute()
+    {
+        $product = new Product();
+
+        $this->assertNull($product->getColor());
+    }
+
+    public function testProductWithRedColorAttribute()
+    {
+        $product = new Product();
+
+        $product->setColor('Red');
+        $this->assertSame('Red', $product->getColor());
+    }
+
+    public function testProductWithBlueColorAttribute()
+    {
+        $product = new Product();
+
+        $product->setColor('Blue');
+        $this->assertSame('Blue', $product->getColor());
+    }
+
+    public function testProductWithGreenColorAttribute()
+    {
+        $product = new Product();
+
+        $product->setColor('Green');
+        $this->assertSame('Green', $product->getColor());
+    }
+}
